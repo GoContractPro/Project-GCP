c2a26ee3be21eb095831e01ceb128ab5436a4a63
Stephen Levenhagen <stephen@stephen-Latitude-D820.(none)>
2015-02-10 15:54:08 -0500

    Revert "Work description field made dynamic to be generated by work lines in task"
    This reverts commit 9cc47a90c9bb9e81ec61d3ad2db29d4d3dd01fd0.

--- npg_project/project_task.py
+++ npg_project/project_task.py
@@ -2,35 +2,13 @@
 from openerp.tools.translate import _
 from openerp.osv import fields, osv
 from openerp import tools
-import math
 
 class task(osv.osv):
     _name = "project.task"
     _inherit = ["project.task"]
-
-    def _get_work_dtl(self, cr, uid, ids, fields, args, context=None):
-        res = {}
-        for task in self.browse(cr, uid, ids, context=context):
-            al = []
-            for wline in task.work_ids:
-                time_spent = ''
-                hrs = wline.hours
-                h = math.floor(hrs)
-                m = (hrs-h)*60
-                wdtl = ""
-                wdtl += 'Date : ' + wline.date
-                wdtl += '\tTime Spent : ' + str(int(h))+':'+str(int(m))
-                wdtl += '\tDone by : ' + wline.user_id.name
-                wdtl += '\nSummary : ' + (wline.name or '')
-                wdtl += wline.wrk_dtl and ('\nWork Detail : ' + wline.wrk_dtl) or ''
-                al.append(wdtl)
-            res[task.id] = "\n\n===================================================\n\n".join(al)
-        return res
-    
     _columns = {
     'task_number':fields.char('Task Number', size=32),
 	'pub_descrip': fields.text('Public Notes'),
-    'work_details': fields.function(_get_work_dtl,string="Work Log Details",type="text",),
     }
 
            
@@ -39,15 +17,8 @@ class task(osv.osv):
             vals['task_number'] = self.pool.get('ir.sequence').get(cr, uid, 'project.task') or '/'
         return super(task, self).create(cr, uid, vals, context=context)
     
-class project_work(osv.osv):
-    _inherit = "project.task.work"
-    _columns = {
-                'wrk_dtl':fields.text("Work Details"),
-                } 
-    _defaults = {
-                 'wrk_dtl':'',
-                 }
 
+    
 
 
 
