12ce3de88b253097c70bdc1cd5ff230787bdd290
Ruby Chauhan <ruby.chauhan@verts.co.in>
2015-04-27 17:59:31 +0530

    resolved issue

---
+++ npg_knowledge_patch/knowledge.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 NovaPoint Group INC (<http://www.novapointgroup.com>)
+#    Copyright (C) 2004-2010 OpenERP SA (<http://www.openerp.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+##############################################################################
+
+import logging
+from openerp.osv import fields, osv
+from openerp import tools
+from openerp.tools.translate import _
+from openerp import netsvc
+from datetime import datetime, timedelta
+import os
+
+_logger = logging.getLogger(__name__)
+
+
+class ir_attachment(osv.osv):
+    _inherit = 'ir.attachment'
+    
+    def _file_read(self, cr, uid, location, fname, bin_size=False):
+        full_path = self._full_path(cr, uid, location, fname)
+        r = ''
+        try:
+            if bin_size:
+                r = os.path.getsize(full_path)
+            else:
+                r = open(full_path,'rb').read().encode('base64')
+        except:
+            _logger.error("_read_file reading %s",full_path)
+        return r
+    
+ir_attachment()
